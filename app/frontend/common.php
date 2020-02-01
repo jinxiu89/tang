f807b9fd2b4fc56e0f905315ef844501d23781a1
kevin <jinxiu89@163.com>
2020-02-01 23:35:35 +0800

    架构设计

---
+++ app/frontend/common.php
@@ -0,0 +1,9 @@
+<?php
+/***
+ * File Name :commmons.php
+ * Create by kevin
+ * Author:jinxiu89@163.com
+ * Create Date :2020/2/1 下午10:44
+ * 人生得意需尽欢，莫使金樽空对月。
+ * 你还记得你吹过的牛逼吗？记住，默默的实现它。
+ ***/
---
+++ app/frontend/route/frontend.php
@@ -0,0 +1,12 @@
+<?php
+/***
+ * File Name :frontend.php
+ * Create by kevin
+ * Author:jinxiu89@163.com
+ * Create Date :2020/2/1 下午10:47
+ * 人生得意需尽欢，莫使金樽空对月。
+ * 你还记得你吹过的牛逼吗？记住，默默的实现它。
+ ***/
+use think\facade\Route;
+
+Route::get('index','index/index');
